cbfb4b3433344f995b7f4d9dc33ae4fced7eab88
Paragatoslr <paragatosleo@gmail.com>
2024-03-05 18:57:34 +0800

    Update index and character

--- resources/views/character.blade.php
+++ resources/views/character.blade.php
@@ -1,5 +1,47 @@
-@extends('template.ui')
+@extends('template.character')
 
 @section('content')
-<h6>Character</h6>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="width: 50%; margin: auto;">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="card">
+                <img src="{{ asset('images/wht-logo.png') }}" class="card-img-top" alt="Image 1">
+                <div class="card-body">
+                    <h5 class="card-title">Card 1</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card">
+                <img src="{{ asset('images/Frog.jpg') }}" class="card-img-top" alt="Image 2">
+                <div class="card-body">
+                    <h5 class="card-title">Card 2</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card">
+                <img src="image3.jpg" class="card-img-top" alt="Image 3">
+                <div class="card-body">
+                    <h5 class="card-title">Card 3</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,78 @@
     </div>
 </div>
 
-<div class="container-fluid" style="height: 50vh"></div> {{-- Gap --}}
+<div class="container-fluid  py-5 ">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-6 bg-light p-lg-5 rounded">
+            <div class="d-flex align-items-start">
+                <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+                    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+                    <button class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" >Disabled</button>
+                    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
+                    <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+                </div>
+                <div class="tab-content" id="v-pills-tabContent">
+
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+                        <h1>sample 1</h1>
+                        <div class="card d-flex flex-row">
+                            <img src="{{ asset('images/Frog.jpg') }}" class="card-img-top" style="width: 50%" alt="Image 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Card 2</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+                        <h1>sample 2</h1>
+                        <div class="card d-flex flex-row">
+                            <img src="{{ asset('images/Frog.jpg') }}" class="card-img-top" style="width: 50%" alt="Image 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Card 2</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-disabled" role="tabpanel" aria-labelledby="v-pills-disabled-tab" tabindex="0">
+                        <h1>sample 3</h1>
+                        <div class="card d-flex flex-row">
+                            <img src="{{ asset('images/Frog.jpg') }}" class="card-img-top" style="width: 50%" alt="Image 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Card 2</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab" tabindex="0">
+                        <h1>sample 4</h1>
+                        <div class="card d-flex flex-row">
+                            <img src="{{ asset('images/Frog.jpg') }}" class="card-img-top" style="width: 50%" alt="Image 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Card 2</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab" tabindex="0">
+                        <h1>sample 5</h1>
+                        <div class="card d-flex flex-row">
+                            <img src="{{ asset('images/Frog.jpg') }}" class="card-img-top" style="width: 50%" alt="Image 2">
+                            <div class="card-body">
+                                <h5 class="card-title">Card 2</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
 
 <div class="container-fluid bg-dark p-5" id="chapters">
     <div class="container ">
